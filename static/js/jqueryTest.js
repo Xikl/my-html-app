bb2167520bf92fed96d0c691307ce115f3533724
X1mo <1392936858@qq.com>
2018-01-18 23:05:46 +0800

    加油！

---
+++ static/js/jqueryTest.js
@@ -0,0 +1,35 @@
+// $(function () {
+//     $("#test1").html("真的是菜")
+// });
+//
+// window.onload = function () {
+//     var p = document.getElementById("imooc");
+//     p.innerHTML = "你真的是菜";
+//     p.style.color = "green";
+// };
+// $(function () {
+//     var p = $("#imooc2")
+//     p.html("你真的是菜的一笔").css('color', 'red');
+// });
+// $(function () {
+//     var $div = $('div') //jQuery对象
+//     var div = $div.get(0) //通过get方法，转化成DOM对象
+//     div.style.color = 'green' //操作dom对象的属性
+//     $div.get(1).style.color = "green"
+// });
+// $(function () {
+//     var a2 = $("#a1")
+//     a2.css('border', '3px solid blue')
+//     var div = document.getElementById("a")
+//     div.style.border = "3px solid red";
+// });
+//
+// $(function () {
+//     $(".test").hidden.html("你好")
+// });
+//
+// $('#test2').click(function () {
+//     //通过包装成jQuery对象改变颜色
+//     $(this).css('color', 'blue');
+// });
+
